516a0dd6a68e10082912f33926deea2969e4221d
Zhanfei Xiao <zfxiao@thoughtworks.com>
2017-09-22 00:14:35 +0530

    Refactor NumbersOperator

--- src/com/thoughtworks/zhanfei/NumbersOperator.java
+++ src/com/thoughtworks/zhanfei/NumbersOperator.java
@@ -6,22 +6,23 @@ import java.util.stream.Collectors;
 
 public class NumbersOperator {
     List<Integer> getPositiveNumbers(List<Integer> initialNumbers) {
-        Predicate<Integer> positive = number -> number > 0;
-
-        return initialNumbers.stream().filter(positive).collect(Collectors.toList());
+        return operate(initialNumbers, number -> number > 0);
     }
 
     List<Integer> getPositiveEvenNumbers(List<Integer> initialNumbers) {
-        Predicate<Integer> positive = number -> number > 0;
-        Predicate<Integer> even = number -> number % 2 == 0;
+//        Predicate<Integer> positive = number -> number > 0;
+//        Predicate<Integer> even = number -> number % 2 == 0;
+//
+//        return operate(initialNumbers, positive.and(even));
 
-        return initialNumbers.stream().filter(positive.and(even)).collect(Collectors.toList());
+        return getEvenNumbers(getPositiveNumbers(initialNumbers));
     }
 
+    List<Integer> getEvenNumbers(List<Integer> initialNumbers) {
+        return operate(initialNumbers, number -> number % 2 == 0);
+    }
 
-    public List<Integer> getEvenNumbers(List<Integer> initialNumbers) {
-        Predicate<Integer> even = number -> number % 2 == 0;
-
+    private List<Integer> operate(List<Integer> initialNumbers, Predicate<Integer> even) {
         return initialNumbers.stream().filter(even).collect(Collectors.toList());
     }
 }
